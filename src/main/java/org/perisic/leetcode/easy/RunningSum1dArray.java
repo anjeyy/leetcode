c1b1704a7df0ffaf16b37902225bb4c6b0863f67
Andjelko Perisic <66070554+anjeyy@users.noreply.github.com>
2020-11-10 19:55:46 +0100

    running sum 1d array

--- src/main/java/org/perisic/leetcode/easy/RunningSum1dArray.java
+++ src/main/java/org/perisic/leetcode/easy/RunningSum1dArray.java
@@ -1,20 +1,35 @@
 package org.perisic.leetcode.easy;
 
-import java.util.stream.IntStream;
+import java.util.Arrays;
 
 public class RunningSum1dArray {
 
+    /**
+     * https://leetcode.com/problems/running-sum-of-1d-array/
+     */
 
     public static void main(String[] args) {
 
+        int[] nums1 = new int[]{1, 2, 3, 4};
+        int[] nums2 = new int[]{1, 1, 1, 1, 1};
+        int[] nums3 = new int[]{3, 1, 2, 10, 1};
+
+        System.out.println("## " + Arrays.toString(runningSum(nums1)));
+        System.out.println("## " + Arrays.toString(runningSum(nums2)));
+        System.out.println("## " + Arrays.toString(runningSum(nums3)));
     }
 
 
-    private static int runningsum(int[] sum) {
+    private static int[] runningSum(int[] nums) {
 
-        return IntStream.range(0, sum.length)
-                        .boxed()
-                        .reduce()
-            ;
+        int[] result = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            int tmp = 0;
+            for (int j = 0; j <= i; j++) {
+                tmp = tmp + nums[j];
+            }
+            result[i] = tmp;
+        }
+        return result;
     }
 }
